33c9cdac910b09717d11f79341c446202af02b2a
Ruan Phablo <ruanphablo778@gmail.com>
2024-09-23 17:45:20 -0300

    Feat: Docker-Compose file

---
+++ src/main/java/com/github/yoruhinda/eletronicCommercer/EletronicCommercerApplication.java
@@ -0,0 +1,13 @@
+package com.github.yoruhinda.eletronicCommercer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicCommercerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicCommercerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yoruhinda/eletronicCommercer/EletronicCommercerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yoruhinda.eletronicCommercer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletronicCommercerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
