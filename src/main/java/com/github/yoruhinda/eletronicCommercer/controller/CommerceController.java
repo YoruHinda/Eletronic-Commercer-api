6e860731914ac3493db4922238728e7d94bdc995
Ruan Phablo <ruanphablo778@gmail.com>
2024-09-25 17:42:35 -0300

    Feat: Create Commerce Controller!

---
+++ src/main/java/com/github/yoruhinda/eletronicCommercer/controller/CommerceController.java
@@ -0,0 +1,22 @@
+package com.github.yoruhinda.eletronicCommercer.controller;
+
+import com.github.yoruhinda.eletronicCommercer.models.Product;
+import com.github.yoruhinda.eletronicCommercer.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommerceController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<Product> findProductById(@PathVariable("id") Long id){
+        if(productService.findById(id) != null) return new ResponseEntity<>(productService.findById(id), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
