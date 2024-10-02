674d9af99c60caf0044292e060c49999c5eff25e
Ruan Phablo <ruanphablo778@gmail.com>
2024-10-02 18:48:00 -0300

    Feat: Add four http methods in controller !

--- src/main/java/com/github/yoruhinda/eletronicCommercer/controller/CommerceController.java
+++ src/main/java/com/github/yoruhinda/eletronicCommercer/controller/CommerceController.java
@@ -5,9 +5,7 @@ import com.github.yoruhinda.eletronicCommercer.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CommerceController {
@@ -15,8 +13,28 @@ public class CommerceController {
     private ProductService productService;
 
     @GetMapping("/product/{id}")
-    public ResponseEntity<Product> findProductById(@PathVariable("id") Long id){
-        if(productService.findById(id) != null) return new ResponseEntity<>(productService.findById(id), HttpStatus.OK);
+    public ResponseEntity<Product> findProductById(@PathVariable("id") Long id) {
+        if (productService.findById(id) != null)
+            return new ResponseEntity<>(productService.findById(id), HttpStatus.OK);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity deleteProductById(@PathVariable("id") Long id) {
+        if (productService.findById(id) != null) {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<Product> postProduct(Product product) {
+        return new ResponseEntity<>(productService.saveProduct(product), HttpStatus.OK);
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, Product product){
+        return new ResponseEntity<>(productService.updateProduct(id, product), HttpStatus.OK);
     }
 }
--- src/main/java/com/github/yoruhinda/eletronicCommercer/services/ProductService.java
+++ src/main/java/com/github/yoruhinda/eletronicCommercer/services/ProductService.java
@@ -1,6 +1,5 @@
 package com.github.yoruhinda.eletronicCommercer.services;
 
-import com.github.yoruhinda.eletronicCommercer.dto.product.ProductDtoRequest;
 import com.github.yoruhinda.eletronicCommercer.models.product.Product;
 import com.github.yoruhinda.eletronicCommercer.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,27 +12,36 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    public Product findById(Long id){
-        if(productRepository.findById(id).isPresent()){
+    public Product findById(Long id) {
+        if (productRepository.findById(id).isPresent()) {
             return productRepository.findById(id).get();
         }
         return null;
     }
 
-    public void deleteProduct(Long id){
-        if (productRepository.findById(id).isPresent()){
+    public Product saveProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        if (productRepository.findById(id).isPresent()) {
             Product product = productRepository.findById(id).get();
             productRepository.delete(product);
         }
     }
 
-    public void updateProduct(Long id, Product products){
-        if (productRepository.findById(id).isPresent()){
+    public Product updateProduct(Long id, Product products) {
+        if (productRepository.findById(id).isPresent()) {
             Product product = productRepository.findById(id).get();
+            product.setProduct_name(products.getProduct_name());
+            product.setProduct_price(products.getProduct_price());
+            product.setProduct_quantity(products.getProduct_quantity());
+            return productRepository.save(product);
         }
+        return null;
     }
 }
