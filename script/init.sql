91a258e05d5f774d19a98b660fb87a901f0b5a2a
Ruan Phablo <ruanphablo778@gmail.com>
2024-10-31 21:48:11 -0300

    Feat: Add User role as standard and init.sql!

---
+++ script/init.sql
@@ -0,0 +1,35 @@
+create database if not exists commercer;
+
+use commercer;
+
+create table if not exists product(
+    id int NOT NULL AUTO_INCREMENT,
+    product_name varchar(255),
+    product_description varchar(255),
+    product_quantity int,
+    product_price float,
+    product_image_name varchar(255),
+    PRIMARY KEY (id)
+);
+
+create table if not exists `user`(
+    id int NOT NULL AUTO_INCREMENT,
+    username varchar(255),
+    password varchar(255),
+    userRole varchar(255),
+    PRIMARY KEY (id)
+);
+
+insert into `user` (username, password, userRole)
+values
+('admin', 'admin', 'ADMIN');
+
+insert into product (product_name, product_description, product_price, product_quantity, product_image_name)
+values
+("Teclado", "Teclado gamer", 299.90, 9, "teclado.png"),
+("Mause", "Mause gamer", 199.90, 8, "mause.png"),
+("Headset", "Headset gamer", 199.90, 2, "headset.png"),
+("Celular", "Celular gamer", 1199.90, 1, "celular.png"),
+("Monitor", "Monitor gamer", 2699.90, 5, "monitor.jpg"),
+("MausePad", "MausePad gamer", 99.90, 3, "mausepad.png"),
+("Playstation 5 Pro", "Playstation gamer", 1999.90, 2, "ps5.png");
