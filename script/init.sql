4c2e13ce374bf12296ffbfe8a9ce296d2a8c2a49
Ruan Phablo <ruanphablo778@gmail.com>
2024-11-01 20:08:15 -0300

    Fix: Init.sql and Config.

--- script/init.sql
+++ script/init.sql
@@ -1,30 +1,26 @@
-create database if not exists commercer;
+create database if not exists `commerce`;
 
-use commercer;
+use `commerce`;
 
-create table if not exists product(
-    id int NOT NULL AUTO_INCREMENT,
-    product_name varchar(255),
-    product_description varchar(255),
-    product_quantity int,
-    product_price float,
-    product_image_name varchar(255),
-    PRIMARY KEY (id)
+create table if not exists `product`(
+    `id` int NOT NULL AUTO_INCREMENT,
+    `product_name` varchar(255),
+    `product_description` varchar(255),
+    `product_quantity` int,
+    `product_price` float,
+    `product_image_name` varchar(255),
+    PRIMARY KEY (`id`)
 );
 
-create table if not exists `user`(
-    id int NOT NULL AUTO_INCREMENT,
-    username varchar(255),
-    password varchar(255),
-    userRole varchar(255),
-    PRIMARY KEY (id)
+create table if not exists `users`(
+    `id` int NOT NULL AUTO_INCREMENT,
+    `username` varchar(255),
+    `password` varchar(255),
+    `user_role` varchar(255),
+    PRIMARY KEY (`id`)
 );
 
-insert into `user` (username, password, userRole)
-values
-('admin', 'admin', 'ADMIN');
-
-insert into product (product_name, product_description, product_price, product_quantity, product_image_name)
+insert IGNORE into `product` (`product_name`, `product_description`, `product_price`, `product_quantity`, `product_image_name`)
 values
 ("Teclado", "Teclado gamer", 299.90, 9, "teclado.png"),
 ("Mause", "Mause gamer", 199.90, 8, "mause.png"),
